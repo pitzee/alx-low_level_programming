36ab27d73be8730d1670fe59c0de3fae895aa265
pitzee <pitzee421@gmail.com>
2022-12-12 03:56:03 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints string to the standard error.
+ *
+ *Return: Always 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar");
+
+	return (1);
+
+}
