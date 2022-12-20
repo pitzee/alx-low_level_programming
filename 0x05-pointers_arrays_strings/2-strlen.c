31f528377ddcd19836077d8e758a185ef33e7e3e
pitzee <pitzee421@gmail.com>
2022-12-20 12:53:56 -0800

    2-sterlin

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,11 +3,14 @@
  *_strlen - return the length of astringth
  *
  * @s: hold the string length
+ *
+ *Return: i
  */
 int _strlen(char *s)
 {
-	int i=0;
-	while(*s != '\0')
+	int i = 0;
+
+	while (*s != '\0')
 	{
 		i++;
 		s++;
