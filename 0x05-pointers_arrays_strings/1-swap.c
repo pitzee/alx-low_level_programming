5bcc8b3fc8d1d2a37fcbebb8fc1b478686b5e680
pitzee <pitzee421@gmail.com>
2022-12-20 04:12:02 -0800

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers
+ *
+ *@a: it is the value going to be sawped to b
+ *
+ *@b: it is the value going to be swaped to a
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
