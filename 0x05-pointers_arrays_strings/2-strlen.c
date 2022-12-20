d29519dd2f8d8c5cda695b9dc9f36f3f3523c4d6
pitzee <pitzee421@gmail.com>
2022-12-20 12:46:51 -0800

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen - return the length of astringth
+ *
+ * @s: hold the string length
+ */
+int _strlen(char *s)
+{
+	int i=0;
+	while(*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
