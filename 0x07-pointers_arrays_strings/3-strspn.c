252ecb6885da95de743de7d8aae11761ab2727d0
pitzee <pitzee421@gmail.com>
2023-01-03 09:56:21 -0800

    task4_1

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (i = 0; *(accept + i); i++)
 		{
-			if (*(s + j) == (accept + i))
+			if (*(s + j) == *(accept + i))
 				break;
 		}
 	if (*(accept + i) == '\0')
