612be2b1fe161176b29447df9edc8de160907628
pitzee <pitzee421@gmail.com>
2023-02-05 09:56:43 -0800

    linked -list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,21 +8,18 @@
  * Return: return length
  */
 
+
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	if (h == NULL)
+	while (h)
 	{
+	if (!h->str)
 	printf("[0] (nil)\n");
-	}
-
-	struct list_s *ptr = h;
-
-	while (h != NULL)
-	{
-	printf("[%u] %s\n", ptr->len, ptr->str);
-	ptr = ptr->next;
+	else
+	printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
 	s++;
 	}
 
