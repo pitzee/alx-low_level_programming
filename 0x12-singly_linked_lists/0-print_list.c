4f3afe4143a87c07414f994aa7d3ecd47e6f8b7c
pitzee <pitzee421@gmail.com>
2023-02-05 10:14:31 -0800

    linked-list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,11 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	if (h == NULL)
+	while (h)
 	{
+	if (!h->str)
 	printf("[0] (nil)\n");
-	}
-
-	while (h != NULL)
-	{
+	else
 	printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
 	s++;
