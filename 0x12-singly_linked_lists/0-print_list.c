6d2855d5d5d29e3de288f4711f9a2e45b1120b85
pitzee <pitzee421@gmail.com>
2023-02-05 10:05:22 -0800

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,14 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (h)
+	if (h == NULL)
 	{
-	if (!h->str)
 	printf("[0] (nil)\n");
-	else
-	printf("[%u] %s\n", h->len, h->str);
-	h = h->next;
+	}
+	struct list_s *ptr = h;
+	{
+	printf("[%u] %s\n", ptr->len, ptr->str);
+	ptr = ptr->next;
 	s++;
 	}
 
