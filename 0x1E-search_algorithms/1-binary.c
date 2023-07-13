32a9cf5bcc1f5a58110f41eb24c952600c91db44
pitzee <pitzee421@gmail.com>
2023-07-13 04:36:07 -0700

    binary_search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,33 +1,56 @@
 #include "search_algos.h"
 
-void print_array(int *array, size_t left, size_t right)
+/**
+ * print_array - prints the array of ints, separated with commas
+ *
+ * @array: a pointer to the array to print
+ * @first: the first index to print
+ * @last: the last index to print
+ */
+
+void print_array(int *array, size_t first, size_t last)
 {
-    printf("Searching in array: ");
-    for (size_t i = left; i <= right; i++)
-        printf("%d ", array[i]);
-    printf("\n");
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = first; i <= last; i++)
+	{
+		if (i != first)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
 }
+
+/**
+ * binary_search - searches for a value in an array of integers
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the index where value is located or -1 on failure or not found
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
-    if (!array)
-        return (-1);
+	size_t i = 0, first = 0, last = size - 1;
 
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+	if (array)
+	{
+		while (first <= last)
+		{
+			print_array(array, first, last);
+			i = (first + last) / 2; /* Gets the middle value */
 
-    print_array(array, left, right);
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-        print_array(array, left, right);
-    }
-    return (-1);
+			if (value > array[i]) /* Uses the right part of array */
+				first = i + 1;
+			else if (value < array[i]) /* Uses the left part of array */
+				last = i - 1;
+			else
+				return (i);
+		}
+	}
+
+	return (-1);
 }
